653e5143c2de7f11a66137717d4f60d3c06b34e8
numselli <58607232+numselli@users.noreply.github.com>
2023-02-18 13:02:40 -0800

    fix resumes not "ready-ing" client

--- lib/gateway/Shard.ts
+++ lib/gateway/Shard.ts
@@ -489,6 +489,7 @@ export default class Shard extends TypedEmitter<ShardEvents> {
                 }
                 this.status = "ready";
                 this.client.shards["_ready"](this.id);
+                void this.checkReady();
                 this.emit("resume");
                 break;
             }
