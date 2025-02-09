fef0825ab63911c075fe568bdb6a4a82643625a8
numselli <58607232+numselli@users.noreply.github.com>
2025-02-08 17:22:31 -0800

    Revert "try to fix"
    This reverts commit 22ddff6843e8015715c5e5d2eedf824996e00827.

--- esm.mjs
+++ esm.mjs
@@ -1,6 +1,6 @@
 // Channel and Interaction MUST be at the top due to circular imports
-const Client = (await import("./lib/Client.js")).default;
-const Permission = (await import("./lib/structures/Permission.js")).default;
+const Client = (await import("./dist/lib/Client.js")).default;
+const Permission = (await import("./dist/lib/structures/Permission.js")).default;
 
 export {
     Client,
